287c574a520d6da85dd3019a5a257250f5fc9d03
Jhadira LC <jhadiraalison.lavado@gmail.com>
2024-09-19 20:47:46 -0500

    Añadiendo archivos

---
+++ Nosotras.js
@@ -0,0 +1,13 @@
+PELICULAS JS
+cuando la pagina cargue
+en el ajax en el succes esta la function parentesis data. los datos es todo lo que llamaremos de la api, pero para tener un mejor orden estos datos los almacenaremos en un array q es filmsData q es igual a los datpos q hay en la api (q es de peliculas)
+
+
+PERSONAJES 3
+aqui no estamos usando la api, pero si un archivo json q leti ha creado
+lo q se detalla en la url la ruta al .json
+
+en esta parte del error 
+
+LOCACIONES 2
+
